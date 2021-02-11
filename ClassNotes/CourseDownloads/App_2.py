78e259eb411fe50eeea14a519f910f8cd7b1c5f4
minefarmer <pgoldfarm@yahoo.com>
2021-02-10 23:00:36 -0600

    ClassNotes: good night!

---
+++ ClassNotes/CourseDownloads/App_2.py
@@ -0,0 +1,72 @@
+
+# ============================================
+# @name BEGINNING PYTHON 3
+# @author J. Djimitry Riviere
+# @description Easy, simple introduction to the
+#              Python 3 programming language for
+#              those who want to start coding.
+# @date 06-11-2018
+#
+# UNIT 2
+# Activity 01 - Data Types
+#
+# NOTE:
+# You don't have to change anything from the
+# code below. This is an example I left for you
+# to evaluate and use as a reference.
+#
+# SIDENOTE:
+# The advantage of Python variables is that you
+# don't have to declare its type, as you would
+# do with other more complex programming languages.
+# For instance, let's compare Java and Python:
+# 
+# --- JAVA ---  |  --- PYTHON ---
+# int a = 30;   |  a = 30
+#
+# *Which one would you rather choose? ;)
+#
+# In any programming languages, variables should always be
+# declared, first, and then attribute it a value, so that it
+# can be used. Some languages even permit you to declare a
+# variable without giving it a value right away. Unfortunately,
+# this is NOT the case for Python!
+# 
+# --- JAVA ---  | --- PYTHON ---
+# int a;        | a <-- This is NOT allowed! 
+# a = 30;       | a = 30
+# ============================================
+a = 30
+b = "Hello"
+c = 3.5
+d = True
+
+# The method type(value) allows you to determine what kind of data type
+# a variable is.
+print("Value of a:", a, " - Type of value a:", type(a))
+print("Value of b:", b, " - Type of value b:", type(b))
+print("Value of c:", c, " - Type of value c:", type(c))
+print("Value of d:", d, " - Type of value d:", type(d))
+
+# --------------------------------------------#
+# ASSIGNMENT:
+# Create as many variables as you want and give
+# them each a value. Then determine the type of
+# each value, using the method type().
+# Try to create a value like the Java example to
+# see if it works. Do you receive an error, or is it
+# allowed? What happens when you create a variable but
+# you don't give it a value right away, then try to
+# print it?
+#
+# OBJECTIVE:
+# Although one doesn't have to tell Python what type
+# of value a variable is (like other more complex
+# programming languages), Python is smart enough to determine
+# it and remember it for you. But what if you forget it, or
+# you can't see where and how it was declared? Simply use
+# the type() method!
+#
+# --> START YOUR ASSIGNMENT BELOW! <--
+# --------------------------------------------
+
---
+++ ClassNotes/Unit3/Lists.py
@@ -0,0 +1,59 @@
+# list_a = [1, "hello", 3.6, True]  # [1, 'hello', 3.6, True]
+
+# print(list_a)  # [1, 'hello', 3.6, True]
+# print("Length of list_A:", len(list_a))  # Length of list_A: 4
+
+# # print(list_a[srart:end:skip])
+# print(list_a[1])  # hello
+# print(list_a[1:])  # ['hello', 3.6, True]
+# print(list_a[:2])  # [1, 'hello']
+# print(list_a[1:3])  # ['hello', 3.6]
+# print(list_a[::2])  # [1, 3.6]
+
+
+# tuple_a = ("World", False, 7, 2.5)
+# print(tuple_a)  # ('World', False, 7, 2.5)
+# print(tuple_a[1])  # False
+# print(tuple_a[1:])  # (False, 7, 2.5)
+# print(tuple_a[:2])  # ('World', False)
+# print(tuple_a[1:3])  # (False, 7)
+# print(tuple_a[::2])  # [1, 3.6]
+
+
+# dict_a = { key:value, key2:value2, ... }
+dict_a = {"name": "CRM", "age": 82, "is_awesome": True, 5: "Yes"}
+
+print(dict_a["name"])  # CRM
+print(dict_a[5])  # Yes
+
+
+
+# tuple_a = ("World", False, 7, 2.5)
+# print(tuple_a)  # ('World', False, 7, 2.5)
+# print(tuple_a[1])  # False
+# print(tuple_a[1:])  # (False, 7, 2.5)
+# print(tuple_a[:2])  # ('World', False)
+# print(tuple_a[1:3])  # (False, 7)
+# print(tuple_a[::2])  # [1, 3.6]
+
+
+
+
+# Strings
+str_a = "Welcome to My Python Class"
+print(str_a[1])  # e
+print(str_a[1:])  # elcome to My Python Class
+print(str_a[:2])  # We
+print(str_a[1:3])  # el
+print(str_a[::2])  # Wloet yPto ls
+
+
+'''
+>>> list_a = [1, "hello", 3.6, True]
+>>> list_a
+[1, 'hello', 3.6, True]
+>>> len(list_a)
+4
+'''
+
+
